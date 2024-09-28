4741e602d38edea494320f12f93b225f77bc32ac
VadimBarinov <barinovvadim87@gmail.com>
2024-09-28 16:47:56 +0400

    RLE

---
+++ 1-decode-RLE.py
@@ -0,0 +1,28 @@
+def restore(src):
+    result = ""
+    if src:
+        # Номер счетчика повторений в байтовом массиве
+        i = 0
+        while i < len(src):
+            # Если имеется 1 в верхнем бите
+            if(src[i] >= 128):
+                # Добавление цепочки одинаковых байтов
+                result += str(chr(src[i+1])) * (src[i] - 128)
+                i += 2
+            # Если в верхнем бите 0
+            else:
+                for j in range(1, src[i] + 1):
+                    # Добавление последовательности неповторяющихся байтов
+                    result += str(chr(src[i+j]))
+                i += src[i] + 1
+                
+    return result
+    
+
+if __name__ == "__main__":
+    compressed = open('1-compressed.txt', 'rb')
+    string = compressed.read()
+    restored = open('1-restored.txt', 'w')
+    restored.write(restore(string))
+    compressed.close()
+    restored.close()
---
+++ 1-encode-RLE.py
@@ -0,0 +1,58 @@
+def rle(src):
+    # Байтовый массив для результата
+    result = bytearray()
+    if src:
+        # Текущий байт
+        current = str(src[0])
+        # Счетчик повторяющихся байтов
+        counterDouble = 1
+        # Последовательность неповторяющихся байтов
+        subsequence = ''
+
+        for e in src[1:]:
+            if e == current and counterDouble < 127:
+                counterDouble += 1
+                # Добавление последовательности неповторяющихся байтов
+                if (len(subsequence) > 1): 
+                    subsequence = subsequence[:-1]
+                    result.extend(bytes([len(subsequence)]))
+                    result.extend((subsequence.encode('utf-8')))
+                subsequence = ""
+            elif counterDouble > 1:
+                # Добавление цепочки одинаковых байтов
+                subsequence += e
+                result.extend(bytes([counterDouble + 128]))
+                result.extend((current.encode('utf8')))
+                current = e
+                counterDouble = 1
+            else:
+                if (len(subsequence) < 127):    
+                    subsequence += e
+                    current = e
+                else:
+                    # Добавление последовательности неповторяющихся байтов
+                    result.extend(bytes([len(subsequence)]))
+                    result.extend((subsequence.encode('utf-8')))
+                    subsequence = e
+                    current = e
+
+        if (counterDouble > 1):
+            # Добавление цепочки одинаковых байтов
+            result.extend(bytes([counterDouble + 128]))
+            result.extend((current.encode('utf8')))
+        else:
+            # Добавление последовательности неповторяющихся байтов
+            result.extend(bytes([len(subsequence)]))
+            result.extend((subsequence.encode('utf-8')))
+
+    return result
+
+
+if __name__ == "__main__":
+
+    original = open('1-original.txt', 'r')
+    string = original.read()
+    compressed = open('1-compressed.txt', 'wb')
+    compressed.write(rle(string))
+    original.close()
+    compressed.close()
