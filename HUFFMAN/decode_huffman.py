e35521bdb5ab53e8e2791e6b4e21abfb571240cb
VadimBarinov <barinovvadim87@gmail.com>
2024-10-17 16:28:15 +0400

    refactoring

--- HUFFMAN/decode_huffman.py
+++ HUFFMAN/decode_huffman.py
@@ -1,18 +1,20 @@
 from struct import *  
 import pickle
 
-# аргументы: (закодированная строка, словарь)
-def huffman_decode(encoded, code):  # функция декодирования исходной строки по кодам Хаффмана
-    sx =[]  # инициализируем массив символов раскодированной строки
-    enc_ch = ""  # инициализируем значение закодированного символа
-    for ch in encoded:  # обойдем закодированную строку по символам
-        enc_ch += ch  # добавим текущий символ к строке закодированного символа
-        for dec_ch in code:  # постараемся найти закодированный символ в словаре кодов
+
+# функция декодирования(закодированная строка, словарь)
+def huffman_decode(encoded, code):
+    sx =[] # раскодированная строка
+    enc_ch = ""  # значение символа
+    for ch in encoded:
+        enc_ch += ch
+        for dec_ch in code:
             if code.get(dec_ch) == enc_ch:  # если закодированный символ найден,
-                sx.append(dec_ch)  # добавим значение раскодированного символа к массиву раскодированной строки
+                sx.append(dec_ch)  # добавим значение символа
                 enc_ch = ""  # обнулим значение закодированного символа
                 break
-    return "".join(sx)  # вернем значение раскодированной строки
+    return "".join(sx)
+
 
 def main():
     input_file = open("HUFFMAN/compressed.huff", "rb")
@@ -26,13 +28,15 @@ def main():
         if len(rec) != 2:
             break
         (data, ) = unpack('>H', rec)
+        # формируем входную строку
         encoded += "0" * (16 - len(bin(data)[2:])) + str(bin(data)[2:])
 
     code = pickle.load(dictionary_file)
+    # убираем ненужные нули из конца строки
     encoded = encoded[:(len(encoded) - int(code['zero_counter']))]
+    print(encoded)
 
     del code['zero_counter']
-    print(encoded)
 
     output_string = huffman_decode(encoded, code)
     output_file.write(output_string)
@@ -40,6 +44,7 @@ def main():
     input_file.close()
     dictionary_file.close()
     output_file.close()
+
 
 if __name__ == "__main__":
     main()
--- HUFFMAN/encode_huffman.py
+++ HUFFMAN/encode_huffman.py
@@ -1,72 +1,87 @@
-import heapq  # модуль для работы с мин. кучей из стандартной библиотеки Python
-from collections import Counter  # словарь в котором для каждого объекта поддерживается счетчик
+import heapq
+from collections import Counter
 from collections import namedtuple
 from struct import *  
 import pickle
 
 
-# добавим классы для хранения информации о структуре дерева
-# воспользуемся функцией namedtuple из стандартной библиотеки
-class Node(namedtuple("Node", ["left", "right"])):  # класс для ветвей дерева - внутренних узлов; у них есть потомки
+# класс для ветвей дерева - внутренних узлов; у них есть потомки
+class Node(namedtuple("Node", ["left", "right"])):
     def walk(self, code, acc):
-        # чтобы обойти дерево нам нужно:
-        self.left.walk(code, acc + "0")  # пойти в левого потомка, добавив к префиксу "0"
-        self.right.walk(code, acc + "1")  # затем пойти в правого потомка, добавив к префиксу "1"
+        # обход дерева
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
 
-class Leaf(namedtuple("Leaf", ["char"])):  # класс для листьев дерева, у него нет потомков, но есть значение символа
+
+# класс для листьев дерева, у него нет потомков, но есть значение символа
+class Leaf(namedtuple("Leaf", ["char"])):
     def walk(self, code, acc):
-        # потомков у листа нет, по этому для значения мы запишем построенный код для данного символа
-        code[self.char] = acc or "0"  # если строка длиной 1 то acc = "", для этого случая установим значение acc = "0"
+        # если строка длиной 1 то acc = "", для этого случая установим значение acc = "0"
+        code[self.char] = acc or "0"
 
-def huffman_encode(s):  # функция кодирования строки в коды Хаффмана
-    h = []  # инициализируем очередь с приоритетами
-    for ch, freq in Counter(s).items(): # постоим очередь с помощью цикла, добавив счетчик, уникальный для всех листьев
-        h.append((freq, len(h), Leaf(ch)))  # очередь будет представлена частотой символа, счетчиком и самим символом
-    heapq.heapify(h)  # построим очередь с приоритетами
-    count = len(h) # инициализируем значение счетчика длиной очереди
-    while len(h) > 1:  # пока в очереди есть хотя бы 2 элемента
+
+# функция кодирования строки в коды Хаффмана
+def huffman_encode(s):
+    h = []  # очередь
+
+    # построим очередь
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+
+    # построим очередь с приоритетами
+    heapq.heapify(h)
+    count = len(h)
+
+    while len(h) > 1:
         freq1, _count1, left = heapq.heappop(h)  # вытащим элемент с минимальной частотой - левый узел
         freq2, _count2, right = heapq.heappop(h)  # вытащим следующий элемент с минимальной частотой - правый узел
-        # поместим в очередь новый элемент, у которого частота равна суме частот вытащенных элементов
-        heapq.heappush(h, (freq1 + freq2, count, Node(left, right))) # добавим новый внутренний узел у которого
-                                                                     # потомки left и right соответственно
-        count += 1  # инкрементируем значение счетчика при добавлении нового элемента дерева
-    code = {}  # инициализируем словарь кодов символов
-    if h:  # если строка пустая, то очередь будет пустая и обходить нечего
-        [(_freq, _count, root)] = h  # в очереди 1 элемент, приоритет которого не важен, а сам элемент - корень дерева
-        root.walk(code, "")  # обойдем дерева от корня и заполним словарь для получения кодирования Хаффмана
-    return code  # возвращаем словарь символов и соответствующих им кодов
+        # поместим в очередь новый элемент, у которого частота равна сумме частот вытащенных элементов
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+
+    code = {}  # словарь кодов символов
+    if h:  # если строка существует
+        # в очереди 1 элемент, приоритет которого не важен, а сам элемент - корень дерева
+        [(_freq, _count, root)] = h
+        # обойдем дерева от корня и заполним словарь
+        root.walk(code, "")
+
+    return code
+
 
 def main():
     input_file = open("HUFFMAN/original.txt", "r")
+    # input_file = open("HUFFMAN/The two brothers.txt", "r")
+
     output_file = open("HUFFMAN/compressed.huff", "wb")
     dictionary_file = open("HUFFMAN/dictionary.pkl", "wb")
 
-    # чтение исходного файла
-    s = input_file.read()
+    s = input_file.read() # чтение исходного файла
 
-    code = huffman_encode(s)  # кодируем строку
-    encoded = "".join(code[ch] for ch in s)  # отобразим закодированную версию, отобразив каждый символ
-                                             # в соответствующий код и конкатенируем результат
-    print(len(code), len(encoded))  # выведем число символов и длину закодированной строки
-    for ch in sorted(code): # обойдем символы в словаре в алфавитном порядке с помощью функции sorted()
-        print("{}: {}".format(ch, code[ch]))  # выведем символ и соответствующий ему код
-    print(encoded)  # выведем закодированную строку
+    code = huffman_encode(s)  # получаем словарь
+    encoded = "".join(code[ch] for ch in s)  # закодируем каждый символ
+
+    print(encoded)
+    # выведем число символов и длину закодированной строки
+    print(len(code), len(encoded))
+    for ch in sorted(code):
+        # выведем символ и соответствующий ему код
+        print("{}: {}".format(ch, code[ch]))
 
     len_encoded = len(encoded) # длина закодированной строки
     zero_counter = 16 - len_encoded % 16 # сколько не хватает до 16 в конце строки
     encoded += '0' * zero_counter
-    for i in range(0, len(encoded), 16):
+    for i in range(0, len(encoded), 16): # запись в файл закодированной строки
         output_file.write(pack('>H', int(encoded[i:i+16], 2)))
 
-    # добавление в конец словаря элемент, показывающий количество незначящих нулей в конце строки
+    # добавление в конец словаря элемента, показывающего количество незначящих нулей в конце строки
     code['zero_counter'] = zero_counter
-    # запись в файл словаря
-    pickle.dump(code, dictionary_file)
+    pickle.dump(code, dictionary_file) # запись словаря в файл
  
     input_file.close()
     output_file.close()
     dictionary_file.close()
+
 
 if __name__ == "__main__":
     main()
